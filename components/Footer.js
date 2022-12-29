6af4898d050b5feebb9d7121badc2f168878a8c9
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-12-29 11:51:34 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Made for students by students
+        Copyright © 2022. All Rights Reserved.
       </footer>
     </>
   )
